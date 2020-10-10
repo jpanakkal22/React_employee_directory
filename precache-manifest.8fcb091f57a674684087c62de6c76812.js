310fea30c6aaed7f42550475e053a44aba577f89
New Dad <josh_panakkal3122@hotmail.com>
2020-10-10 19:21:00 -0400

    Updates

---
+++ precache-manifest.8fcb091f57a674684087c62de6c76812.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8b6aaf74f155fbadccdb2821a944f49",
+    "url": "/React_employee_directory/index.html"
+  },
+  {
+    "revision": "ec63d6307188623e6c2d",
+    "url": "/React_employee_directory/static/css/main.efe68409.chunk.css"
+  },
+  {
+    "revision": "8e0ce574cec557fcc99b",
+    "url": "/React_employee_directory/static/js/2.a791cc41.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React_employee_directory/static/js/2.a791cc41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec63d6307188623e6c2d",
+    "url": "/React_employee_directory/static/js/main.65281644.chunk.js"
+  },
+  {
+    "revision": "9524b6edf97b929bc5df",
+    "url": "/React_employee_directory/static/js/runtime-main.c094c421.js"
+  }
+]);
